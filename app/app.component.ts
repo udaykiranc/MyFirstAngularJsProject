0e3ee8866b4203ad33c1e81d9e3488efd4f8c733
Uday Kiran <uday.kiran@paysafe.com>
2016-12-04 22:36:34 +0530

    First working version of this project

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,20 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>`,
+  template: `<h1>Hello {{name}}</h1>`,
 })
-
-export class AppComponent {
-  title = 'Tour of Heroes';
-  hero = 'Windstorm';
-}
-
-export class Hero {
-	id : number;
-	name : string;
-}
-
-hero : Hero {
-	id : 1,
-	name : 'Windstorm!'
-};
+export class AppComponent  { name = 'Angular'; }
