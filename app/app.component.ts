2b022c0845bac5db24b27063f4e903991b4bd2e3
Uday Kiran <uday.kiran@paysafe.com>
2016-12-04 23:22:11 +0530

    deleted unnecessary files

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: 
+    `<h1>Hello {{title}}</h1>
+    <h2>{{hero.name}} details!</h2> 
+    <div><label>id :</label>{{hero.id}}</div> 
+    <div>
+      <label>name :</label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Angular'; 
+  title = 'Tour of Heroes';
+  hero : Hero = {
+	id : 1,
+	name : 'Uday'
+  }
+}
+
+export class Hero {
+	id : number;
+	name : string;
+}
