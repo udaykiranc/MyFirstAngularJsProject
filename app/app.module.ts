caa1110e7d75ac792d53ccd4dec07d4f64f73a0e
udaykiran chinthakindi <udaykiran chinthakindi>
2016-12-10 17:05:26 +0530

    working version after partial completion of routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,37 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {HeroDetailComponent} from './hero-detail.component';
 
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
 import { AppComponent }  from './app.component';
+import { HeroService } from './hero.service';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+	BrowserModule, 
+	FormsModule,
+	RouterModule.forRoot([
+		{
+			path : 'heroes',
+			component : HeroesComponent
+		}
+	])
+  ],
+  declarations: [ 
+	HeroesComponent, 
+	HeroDetailComponent,
+	AppComponent
+  ],
+  providers: [
+    HeroService
+  ],				
+  bootstrap:    [ 
+	AppComponent 
+  ]
 })
+
+
 export class AppModule { }
